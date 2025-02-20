1ec1849fe84ec348863cf83097561a2d68343234
NixsDK <niksdanielskalnins@gmail.com>
2025-02-20 10:29:04 +0200

    code update design

--- GeneralInquieries.php
+++ GeneralInquieries.php
@@ -1,1 +1,13 @@
-<?php
+<?php include "head.php"; ?>
+
+
+<main>
+    <p style='color: #FFFFFF; background-color: rgba(0, 0, 0, 0.7); padding: 10px;'>Main information</p>
+
+</main>
+
+<?php include "footer.php"; ?>
+
+</body>
+</html>
+
--- HomePage.php
+++ HomePage.php
@@ -2,7 +2,7 @@
 
 
 <main>
-    <p style='color: #FFFFFF; background-color: rgba(0, 0, 0, 0.7); padding: 10px;'>Ropazkalns is located in the beautiful countryside of Latvia, surrounded by lush forests and rolling hills.
+    <p style='color: #FFFFFF; background-color: rgba(0, 0, 0, 0.7); padding: 40px;'>Ropazkalns is located in the beautiful countryside of Latvia, surrounded by lush forests and rolling hills.
         Our passion for the outdoors and our love for nature inspired us to create a place where people can escape from the hustle and bustle of the city and enjoy the tranquility of the countryside.</p>
 
 </main>
